c854b0c7cf85f708a91fd328578247458b317585
Michael Hurley <m_hur@yahoo.com>
2013-09-09 08:57:00 -0400

    move ChessBoard instantiation inside controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,12 +6,10 @@ angular.module('kinghunt.controllers', []).
   controller('LoadCtrl', ['$scope', 'book', function($scope, book) {
     $scope.book = book;
   }]).
-  controller('BoardCtrl', ['$scope', '$location', 'ChessBoard',  function($scope, $location, board) {
+  controller('BoardCtrl', ['$scope', '$location', function($scope, $location) {
     var game = new Chess();
-    $scope.board = board;
     $scope.fen = $location.search().fen;
-
-    $scope.board.position($scope.fen);
+    $scope.board = new ChessBoard('board', $scope.fen);
   }]).
   controller('AboutCtrl', ['$scope', 'version', 'credits',  function($scope, version, credits) {
     $scope.version = version;
